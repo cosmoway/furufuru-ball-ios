3f1595502e481871c395b7edbc8e2daacb1cca6b
Sakano Takeru <sakano@cosmoway.net>
2015-07-07 16:26:06 +0900

    create project

---
+++ furufuru-ball-ios/furufuru-ball-iosTests/furufuru_ball_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  furufuru_ball_iosTests.swift
+//  furufuru-ball-iosTests
+//
+//  Created by 坂野健 on 2015/07/07.
+//  Copyright (c) 2015年 坂野健. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class furufuru_ball_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
