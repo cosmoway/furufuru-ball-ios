99aa323f5acea07db1d70bb3e9bb7df03ff338ba
Sakano Takeru <sakano@cosmoway.net>
2015-07-08 17:54:27 +0900

    今の位置に足すようにする

--- furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
+++ furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
@@ -18,20 +18,20 @@ class GameScene: SKScene {
         Circle.position = CGPointMake(self.frame.midX, self.frame.midY)
         Circle.physicsBody = SKPhysicsBody(circleOfRadius: radius)
         Circle.physicsBody?.affectedByGravity = false
+        
         let myMotionManager = CMMotionManager()
         
         // 更新周期を設定.
-        myMotionManager.accelerometerUpdateInterval = 0.1
+        myMotionManager.accelerometerUpdateInterval = 0.01
         
         // 加速度の取得を開始.
         myMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
-            Circle.position.x = CGFloat(accelerometerData.acceleration.x)
-            Circle.position.y = CGFloat(accelerometerData.acceleration.y)
+            Circle.position.x += CGFloat(accelerometerData.acceleration.x)
+            Circle.position.y += CGFloat(accelerometerData.acceleration.y)
         })
         
         // ShapeNodeの塗りつぶしの色を指定.
         Circle.fillColor = UIColor.greenColor()
-        
         self.addChild(Circle)
         self.backgroundColor = UIColor.blackColor()
     }
