776e44adcfc23ecd005c4fd08d3168fdb672df6e
Sakano Takeru <sakano@cosmoway.net>
2015-07-08 17:00:29 +0900

    端からでないようにしました。

--- furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
+++ furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
@@ -8,23 +8,35 @@
 
 import SpriteKit
 
-let Circle = SKShapeNode(circleOfRadius: 40)
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        var radius = 40 as CGFloat
+        var radius = 30 as CGFloat
         /* Setup your scene here */
-        //let Circle = SKShapeNode(circleOfRadius: radius)
+        let Circle = SKShapeNode(circleOfRadius: radius)
         // ShapeNodeの座標を指定.
         Circle.position = CGPointMake(self.frame.midX, self.frame.midY)
         Circle.physicsBody = SKPhysicsBody(circleOfRadius: radius)
-        Circle.physicsBody?.affectedByGravity = true
-        Circle.physicsBody?.velocity = CGVectorMake(1,1)
+        //重力はfalseにしてあります。
+        Circle.physicsBody?.affectedByGravity = false
+        //跳ね返り空気抵抗などなくする処理
         Circle.physicsBody?.restitution = 1.0
         Circle.physicsBody?.linearDamping = 0
         Circle.physicsBody?.friction = 0
         Circle.physicsBody?.usesPreciseCollisionDetection = true
+        
+        let ballSpeed = 600.0
+        
+        //ボールが跳ね返り動く処理
+        var directionX: Double = 1;
+        let randX = arc4random_uniform(10) + 10
+        let randY = arc4random_uniform(10) + 10
+        let randV = sqrt(Double(randX * randX + randY * randY))
+        let speedX = Double(randX) * ballSpeed / randV
+        let speedY = Double(randY) * ballSpeed / randV
+        Circle.physicsBody!.velocity = CGVectorMake(CGFloat(speedX * directionX), CGFloat(speedY))
+        directionX *= -1
         // ShapeNodeの塗りつぶしの色を指定.
         Circle.fillColor = UIColor.greenColor()
         self.addChild(Circle)
@@ -33,6 +45,5 @@ class GameScene: SKScene {
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-        Circle.position.x++
     }
 }
