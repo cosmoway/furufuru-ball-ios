f3d869705d6792919f9e3629fce78c2e352b78ab
Sakano Takeru <sakano@cosmoway.net>
2015-07-10 10:58:23 +0900

    カテゴリはやめて元の実装に戻しました。

--- furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
+++ furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
@@ -12,14 +12,6 @@ import CoreMotion
 class GameScene: SKScene ,SKPhysicsContactDelegate{
     let categoryA: UInt32 = 0x1 << 0
     var myMotionManager: CMMotionManager?
-    let Circle = SKShapeNode(circleOfRadius: 40)
-    let interval = 0.03
-    //反発力
-    let resilience = 0.9
-    var vp_x = 0.0
-    var vp_y = 0.0
-    var v_x = 0.0
-    var v_y = 0.0
     
     override func didMoveToView(view: SKView) {
         self.physicsWorld.contactDelegate = self
@@ -27,7 +19,7 @@ class GameScene: SKScene ,SKPhysicsContactDelegate{
         self.physicsBody?.contactTestBitMask = categoryA
         var radius = 40 as CGFloat
         /* Setup your scene here */
-        //let Circle = SKShapeNode(circleOfRadius: radius)
+        let Circle = SKShapeNode(circleOfRadius: radius)
         // ShapeNodeの座標を指定.
         Circle.position = CGPointMake(self.frame.midX, self.frame.midY)
         Circle.physicsBody = SKPhysicsBody(circleOfRadius: radius)
@@ -36,46 +28,48 @@ class GameScene: SKScene ,SKPhysicsContactDelegate{
         Circle.physicsBody?.affectedByGravity = false
         
         myMotionManager = CMMotionManager()
-      //  let interval = 0.03
+        let interval = 0.03
         //反発力
-       // let resilience = 0.9
+        let resilience = 0.9
         // 更新周期を設定.
-        myMotionManager!.accelerometerUpdateInterval = interval
-        myMotionManager?.deviceMotionUpdateInterval = 0.03
-       // var vp_x = 0.0
-       // var vp_y = 0.0
+        myMotionManager?.deviceMotionUpdateInterval = interval
+        var vp_x = 0.0
+        var vp_y = 0.0
         // 加速度の取得を開始.
         myMotionManager!.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(data: CMDeviceMotion!, error:NSError!) -> Void in
-            var x = data.userAcceleration.x
-            if (data.userAcceleration.x * 10>=20){
-                x = 5
+            var x = data.userAcceleration.x * 5
+            if (data.userAcceleration.x * 5 >= 9) {
+                x = 7
+            }
+            var y = data.userAcceleration.y * 5
+            if (data.userAcceleration.y * 5 >= 9){
+                y = 7
             }
             //加速の計算
-            self.v_x = self.vp_x + (x + data.gravity.x) * 1000 * self.interval
-            self.v_y = self.vp_y + (data.userAcceleration.y * 10 + data.gravity.y) * 1000 * self.interval
-            
+            var v_x = vp_x + (x + data.gravity.x) * 1000 * interval
+            var v_y = vp_y + (y + data.gravity.y) * 1000 * interval
             var flag = true
-            if (self.v_x*self.v_x+self.v_y*self.v_y>=900 * 900) {
+            if (v_x*v_x+v_y*v_y>=900 * 900) {
                 //self.physicsBody = nil
                 //flag = false
             }
-            self.vp_x = self.v_x
-            self.vp_y = self.v_y
+            vp_x = v_x
+            vp_y = v_y
             //壁に当たったか判定
-         //   if ((Circle.position.x + CGFloat(v_x*interval)) < self.frame.maxX-radius && (Circle.position.x + CGFloat(v_x*interval)) > self.frame.minX+radius || !flag) {
-                self.Circle.position.x = self.Circle.position.x + CGFloat(self.v_x*self.interval)
-        //    } else {
+            if ((Circle.position.x + CGFloat(v_x*interval)) <= self.frame.maxX-radius && (Circle.position.x + CGFloat(v_x*interval)) >= self.frame.minX+radius || !flag) {
+                Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
+            } else {
                 //壁に当たった時の反発
-        //        Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
-        //        vp_x = -vp_x * resilience
-        //   }
-         //   if ((Circle.position.y + CGFloat(v_y*interval)) < self.frame.maxY-radius && (Circle.position.y + CGFloat(v_y*interval)) > self.frame.minY+radius || !flag) {
-                self.Circle.position.y = self.Circle.position.y + CGFloat(self.v_y*self.interval)
-        //    } else {
-       //         Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
-       //         vp_y = -vp_y * resilience
-        //    }
-            })
+                Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
+                vp_x = -vp_x * resilience
+           }
+            if ((Circle.position.y + CGFloat(v_y*interval)) <= self.frame.maxY-radius && (Circle.position.y + CGFloat(v_y*interval)) >= self.frame.minY+radius || !flag) {
+                Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
+            } else {
+                Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
+                vp_y = -vp_y * resilience
+            }
+        })
         
         // ShapeNodeの塗りつぶしの色を指定.
         Circle.fillColor = UIColor.greenColor()
@@ -87,10 +81,6 @@ class GameScene: SKScene ,SKPhysicsContactDelegate{
         /* Called before each frame is rendered */
     }
     func didBeginContact(contact: SKPhysicsContact) {
-        //println("didBeginContact")
-        Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
-        vp_x = -vp_x * resilience
-        Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
-        vp_y = -vp_y * resilience
+        println("didBeginContact")
     }
 }
