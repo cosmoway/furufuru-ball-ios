b1557b5265b2fc48e7e0e0b24a9a3882acc2902b
Sakano Takeru <sakano@cosmoway.net>
2015-07-10 14:11:36 +0900

    変数名を作りカテゴリの部分を削除

--- furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
+++ furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
@@ -9,61 +9,64 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene ,SKPhysicsContactDelegate{
-    let categoryA: UInt32 = 0x1 << 0
+class GameScene: SKScene {
     var myMotionManager: CMMotionManager?
     
     override func didMoveToView(view: SKView) {
-        self.physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.physicsBody?.contactTestBitMask = categoryA
         var radius = 40 as CGFloat
         /* Setup your scene here */
         let Circle = SKShapeNode(circleOfRadius: radius)
         // ShapeNodeの座標を指定.
         Circle.position = CGPointMake(self.frame.midX, self.frame.midY)
         Circle.physicsBody = SKPhysicsBody(circleOfRadius: radius)
-        Circle.physicsBody?.contactTestBitMask = categoryA
         //重力はfalseにしてあります。
         Circle.physicsBody?.affectedByGravity = false
         
         myMotionManager = CMMotionManager()
         let interval = 0.03
         //反発力
-        let resilience = 0.9
+        let resilience = 0.8
         // 更新周期を設定.
         myMotionManager?.deviceMotionUpdateInterval = interval
         var vp_x = 0.0
         var vp_y = 0.0
         // 加速度の取得を開始.
         myMotionManager!.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(data: CMDeviceMotion!, error:NSError!) -> Void in
-            var x = data.userAcceleration.x * 5
-            if (data.userAcceleration.x * 5 >= 9) {
-                x = 7
+            //ユーザが動いた時の加速度が小さい為8倍する
+            var twice = 8.0
+            //ユーザが動いた時の加速度の最大値
+            var max_x = 8.0
+            var max_y = 8.0
+            var x = data.userAcceleration.x * twice
+            if (data.userAcceleration.x * twice >= 10) {
+                x = max_x
             }
-            var y = data.userAcceleration.y * 5
-            if (data.userAcceleration.y * 5 >= 9){
-                y = 7
+            var y = data.userAcceleration.y * twice
+            if (data.userAcceleration.y * twice >= 10){
+                y = max_y
             }
             //加速の計算
             var v_x = vp_x + (x + data.gravity.x) * 1000 * interval
             var v_y = vp_y + (y + data.gravity.y) * 1000 * interval
-            var flag = true
-            if (v_x*v_x+v_y*v_y>=900 * 900) {
-                //self.physicsBody = nil
-                //flag = false
+            var through_flag = true
+            //速度
+            let v = 2000.0
+            if (v_x * v_x + v_y * v_y >= v * v) {
+                self.physicsBody = nil
+                through_flag = false
             }
             vp_x = v_x
             vp_y = v_y
             //壁に当たったか判定
-            if ((Circle.position.x + CGFloat(v_x*interval)) <= self.frame.maxX-radius && (Circle.position.x + CGFloat(v_x*interval)) >= self.frame.minX+radius || !flag) {
+            if ((Circle.position.x + CGFloat(v_x*interval)) <= self.frame.maxX-radius && (Circle.position.x + CGFloat(v_x*interval)) >= self.frame.minX+radius || !through_flag) {
                 Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
             } else {
                 //壁に当たった時の反発
                 Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
                 vp_x = -vp_x * resilience
            }
-            if ((Circle.position.y + CGFloat(v_y*interval)) <= self.frame.maxY-radius && (Circle.position.y + CGFloat(v_y*interval)) >= self.frame.minY+radius || !flag) {
+            if ((Circle.position.y + CGFloat(v_y*interval)) <= self.frame.maxY-radius && (Circle.position.y + CGFloat(v_y*interval)) >= self.frame.minY+radius || !through_flag) {
                 Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
             } else {
                 Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
@@ -79,8 +82,5 @@ class GameScene: SKScene ,SKPhysicsContactDelegate{
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-    }
-    func didBeginContact(contact: SKPhysicsContact) {
-        println("didBeginContact")
     }
 }
