13f17730c3658fa91066e29e98414d77c21062aa
Sakano Takeru <sakano@cosmoway.net>
2015-07-27 18:22:37 +0900

    メッセージを受け取れるようになりました。

--- furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
+++ furufuru-ball-ios/furufuru-ball-ios/GameScene.swift
@@ -11,19 +11,20 @@ import CoreMotion
 
 class GameScene: SKScene, SRWebSocketDelegate{
     var myMotionManager: CMMotionManager?
+    var Circle: SKShapeNode?
     private var webSocketClient: SRWebSocket?
+    var through_flag = true
     
     override func didMoveToView(view: SKView) {
         webSocketConnect()
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        
         var radius = 40 as CGFloat
         /* Setup your scene here */
-        let Circle = SKShapeNode(circleOfRadius: radius)
+        Circle = SKShapeNode(circleOfRadius: radius)
         // ShapeNodeの座標を指定.
-        Circle.position = CGPointMake(self.frame.midX, self.frame.midY)
-        Circle.physicsBody = SKPhysicsBody(circleOfRadius: radius)
+        Circle!.physicsBody = SKPhysicsBody(circleOfRadius: radius)
         //重力はfalseにしてあります。
-        Circle.physicsBody?.affectedByGravity = false
+        Circle!.physicsBody?.affectedByGravity = false
         
         myMotionManager = CMMotionManager()
         let interval = 0.03
@@ -33,19 +34,23 @@ class GameScene: SKScene, SRWebSocketDelegate{
         myMotionManager?.deviceMotionUpdateInterval = interval
         var vp_x = 0.0
         var vp_y = 0.0
-        var through_flag = true
+        
         // 加速度の取得を開始.
         myMotionManager!.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(data: CMDeviceMotion!, error:NSError!) -> Void in
             //ユーザが動いた時の加速度が小さい為8倍する
             var twice = 10.0
             //加速の計算
-            var v_x = vp_x + (data.userAcceleration.x + data.gravity.x) * 1000 * interval
-            var v_y = vp_y + (data.userAcceleration.y + data.gravity.y) * 1000 * interval
+            var v_x = vp_x + (data.userAcceleration.x * twice + data.gravity.x) * 1000 * interval
+            var v_y = vp_y + (data.userAcceleration.y * twice + data.gravity.y) * 1000 * interval
             //速度
             let v = 2000.0
-            if (v_x * v_x + v_y * v_y >= v * v) {
+            if (v_x * v_x >= v * v || v_y * v_y >= v * v) {
                 self.physicsBody = nil
-                through_flag = false
+                vp_x=0;
+                vp_y=0;
+                v_x=0;
+                v_y=0;
+                self.through_flag = false
                 if (self.isOpen()) {
                 let obj: [String:AnyObject] = [
                     "move" : "out"
@@ -57,32 +62,32 @@ class GameScene: SKScene, SRWebSocketDelegate{
             vp_x = v_x
             vp_y = v_y
             //壁に当たったか判定
-            if ((Circle.position.x + CGFloat(v_x*interval)) <= self.frame.maxX-radius && (Circle.position.x + CGFloat(v_x*interval)) >= self.frame.minX+radius || !through_flag) {
-                Circle.position.x = Circle.position.x + CGFloat(v_x*interval)
+            if ((self.Circle!.position.x + CGFloat(v_x*interval)) <= self.frame.maxX-radius && (self.Circle!.position.x + CGFloat(v_x*interval)) >= self.frame.minX+radius || !self.through_flag) {
+                self.Circle!.position.x = self.Circle!.position.x + CGFloat(v_x*interval)
             } else {
                 //壁に当たった時の反発
-                if ((Circle.position.x + CGFloat(v_x * interval)) >= self.frame.minX + radius) {
-                    Circle.position.x = self.frame.maxX - radius
+                if ((self.Circle!.position.x + CGFloat(v_x * interval)) >= self.frame.minX + radius) {
+                    self.Circle!.position.x = self.frame.maxX - radius
                 } else {
-                    Circle.position.x = self.frame.minX + radius
+                    self.Circle!.position.x = self.frame.minX + radius
                 }
                 vp_x = -vp_x * resilience
            }
-            if ((Circle.position.y + CGFloat(v_y*interval)) <= self.frame.maxY-radius && (Circle.position.y + CGFloat(v_y*interval)) >= self.frame.minY+radius || !through_flag) {
-                Circle.position.y = Circle.position.y + CGFloat(v_y*interval)
+            if ((self.Circle!.position.y + CGFloat(v_y*interval)) <= self.frame.maxY-radius && (self.Circle!.position.y + CGFloat(v_y*interval)) >= self.frame.minY+radius || !self.through_flag) {
+                self.Circle!.position.y = self.Circle!.position.y + CGFloat(v_y*interval)
             } else {
-                if ((Circle.position.y + CGFloat(v_y * interval)) >= self.frame.minY + radius) {
-                    Circle.position.y = self.frame.maxY - radius
+                if ((self.Circle!.position.y + CGFloat(v_y * interval)) >= self.frame.minY + radius) {
+                    self.Circle!.position.y = self.frame.maxY - radius
                 } else {
-                    Circle.position.y = self.frame.minY + radius
+                    self.Circle!.position.y = self.frame.minY + radius
                 }
                 vp_y = -vp_y * resilience
             }
         })
         
         // ShapeNodeの塗りつぶしの色を指定.
-        Circle.fillColor = UIColor.greenColor()
-        self.addChild(Circle)
+        Circle!.fillColor = UIColor.greenColor()
+        self.addChild(Circle!)
         self.backgroundColor = UIColor.blackColor()
     }
     
@@ -120,6 +125,10 @@ class GameScene: SKScene, SRWebSocketDelegate{
        // through_flag = true
        // Circle.physicsBody?.affectedByGravity = false
        // self.myMotionManager?.startDeviceMotionUpdates()
+        Circle!.position = CGPointMake(self.frame.midX, self.frame.midY)
+        println(message)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.through_flag = true
     }
     func webSocket(webSocket: SRWebSocket!, didFailWithError error: NSError){
         println("error")
